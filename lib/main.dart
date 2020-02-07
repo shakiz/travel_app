284a70fa10c8457b55d7aef073be61bf65ce2adc
shakiz <activities335@gmail.com>
2020-02-07 15:06:08 +0600

    container and column

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,30 @@ class _TravelState extends State<TravelApp>{
         appBar: AppBar(
           title: Text("Travel Guide"),
         ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.teal
+              ),
+              child: Text("One"),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.white
+              ),
+              child: Text("Two"),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.green
+              ),
+              child: Text("Three"),
+            ),
+          ],
+        )
       ),
     );
   }
