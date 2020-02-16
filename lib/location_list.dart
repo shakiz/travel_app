613cbda0f5f434d8e0b2df6061b6428242cf8e80
shakiz <activities335@gmail.com>
2020-02-16 20:25:20 +0600

    listview item tap and navigation

--- lib/location_list.dart
+++ lib/location_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:travel_app/location_details.dart';
 import 'package:travel_app/models/location.dart';
 import 'package:travel_app/styles.dart';
 
@@ -17,17 +18,30 @@ class LocationList extends StatelessWidget {
       ),
       body: ListView.builder(
         itemCount: this.locationList.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            contentPadding: EdgeInsets.all(10),
-            leading: _itemThumbnail(this.locationList[index]),
-            title: _itemTitle(this.locationList[index]),
-          );
-        },
+        itemBuilder:
+            _listViewItemBuilder, //this builder method wont take any params because its a callback
       ),
     );
   }
 
+  //This will build list item
+  Widget _listViewItemBuilder(BuildContext context, int index) {
+    var location = this.locationList[index];
+    return ListTile(
+      contentPadding: EdgeInsets.all(10),
+      leading: _itemThumbnail(location),
+      title: _itemTitle(location),
+      onTap: () => _onClickListener(context, location),
+    );
+  }
+
+  //list item click listener
+  void _onClickListener(BuildContext context, Location location) {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => LocationDetail(location)));
+  }
+
+  //List item thmubnail generator
   Widget _itemThumbnail(Location location) {
     return Container(
       constraints: BoxConstraints.tightFor(width: 100),
@@ -38,6 +52,7 @@ class LocationList extends StatelessWidget {
     );
   }
 
+  //List item title generator
   Widget _itemTitle(Location location) {
     return Text('${location.name}', style: Styles.textDefault);
   }
