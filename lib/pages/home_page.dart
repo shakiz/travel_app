6343cad70509ce42c29a4465cd65093dfe7d7f05
shakiz <activities335@gmail.com>
2022-02-13 16:57:41 +0600

    home menu part 1

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:travel_app/config/styles.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -18,19 +19,6 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text("Home"),
-          leading: InkWell(
-            child: const IconTheme(
-                data: IconThemeData(color: Colors.white),
-                child: Icon(
-                  Icons.menu,
-                )),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-        ),
         body: body(),
       ),
     );
@@ -41,11 +29,91 @@ class _HomePageState extends State<HomePage> {
       child: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        padding: const EdgeInsets.all(24),
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 12, right: 12),
         color: Colors.white,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [],
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Image.asset(
+                    "assets/icons/menu.png",
+                    height: 42,
+                    width: 42,
+                    color: Colors.black87.withOpacity(0.7),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.topRight,
+                  decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(Radius.circular(21)),
+                      border: Border.all(color: Colors.black12),
+                      color: Colors.brown.withOpacity(0.1)),
+                  padding: const EdgeInsets.all(12),
+                  child: Image.asset(
+                    "assets/icons/user.png",
+                    height: 24  ,
+                    width: 24,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16,),
+            Text(
+              "Where you wanna go?",
+              style: TextStyle(
+                color: Styles.baseTextColor,
+                fontSize: 24,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            const SizedBox(height: 16,),
+            Stack(
+              children: [
+                const Align(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      //border: OutlineInputBorder(),
+                      hintText: 'Search your destination',
+                    ),
+                  ),
+                  alignment: Alignment.centerLeft,
+                ),
+                Align(
+                  child: Wrap(
+                    spacing: 0, // space between two icons
+                    children: <Widget>[
+                      SizedBox(
+                        height: 42,
+                        child: ElevatedButton(
+                          onPressed: () async {
+
+                          },
+                          child: Image.asset(
+                            "assets/icons/search.png",
+                            color: Colors.white,
+                          ),
+                          style: ButtonStyle(
+                              backgroundColor:
+                              MaterialStateProperty.all(Styles.baseColor),
+                              shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                ),
+                              )),
+                        ),
+                      ) // icon
+                    ],
+                  ),
+                  alignment: Alignment.topRight,
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
